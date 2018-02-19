6ce016e0f1c5980c299d43ef9db823f8e2a563e7
Hanna Lee <hannabeasiam@gmail.com>
2018-02-19 15:03:16 -0500

    post get method still need to modify

--- assignment1/course.php
+++ assignment1/course.php
@@ -1,5 +1,12 @@
 <?php 
 //Learn about _SERVER
+if ($_SERVER["REQUEST_METHOD"] != 'POST') {
+  $name = '';
+  $email = '';
+  $details = '';
+  $organization = '';
+}
+
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {//open if statement store all post into array
   //var_dump($_POST);//array(3) { ["name"]=> string(0) "" ["email"]=> string(0) "" ["details"]=> string(0) "" }
   $name = trim(filter_input(INPUT_POST, "name"));
@@ -18,14 +25,18 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {//open if statement store all post in
     $validated = false;
   }
   if($validated != false){
+    header("location:course.php?status=thanks");
+    include("course.php");
     echo "<pre>";
     $email_body = "";
     $email_body .= "Name " .$name ."\n"; //view source will have line break not broswer
     $email_body .= "Email " .$email ."\n";
     $email_body .= "Details " .$details ."\n";
-    $email_body .= "Organization " .$organization ."\n";
+    $email_body .= "Organization " .$organization ."\n";      
     echo $email_body;
     echo "</pre>";
+    exit;
+
   } 
 
 
@@ -45,8 +56,7 @@ include("include/header.php");
 
     if(isset($_GET['status']) && $_GET['status'] == 'thanks') {
       echo "<p>Thanks for the email I&rsquo;ll check out your suggestion shortly!</p>";
-      $name=$_POST['name'];
-      echo $name;
+      include_once("location:course.php?status=thanks");
     } 
 
     else {
@@ -62,30 +72,30 @@ include("include/header.php");
   <!--don't want to submit with get method, since it's really easy to modify
   query string data rather use POST-->
   
-  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+  <form method="POST" action="course.php">
     <table>
       <tr>
         <!--label works with id attribute-->
         <th><label for="name">Name</label></th>
-        <td><input type="text" id="name" name="name" value="<?php if(isset($name)) {echo $name; } ?>"/></td>
+        <td><input type="text" id="name" name="name" value="<?php echo $name; ?>"/></td>
       </tr>
       <tr>
         <th><label for="email">Email</label></th>
-        <td><input type="text" id="email" name="email" value="<?php if(isset($email)) {echo $email;} ?>"/></td>
+        <td><input type="text" id="email" name="email" value="<?php echo $email; ?>"/></td>
       </tr>
       <tr>
         <th><label for="details">Suggest Item Details</label></th>
-        <td><textarea id="details" name="details">Your suggestion<?php if(isset($details)) {echo $details;} ?></textarea></td>
+        <td><textarea id="details" name="details">Your suggestion<?php echo $details; ?></textarea></td>
       </tr>
       <tr>
         <th><label for="organization">Organization</label></th>
         <td>
           <select name="organization" id="organization" size="1">
             <option value="">Choose option..</option>
-            <option value="school" <?php if ((isset($organization)) && $organization == "school"){ echo "selected";} ?> >School</option>
-            <option value="non-profit" <?php if ((isset($organization)) && $organization == "non-profit"){ echo "selected";} ?> >Non-Profit</option>
-            <option value="profit" <?php if ((isset($organization)) && $organization == "profit") {echo "selected";} ?> >Profit</option>
-            <option value="indivisual" <?php if ((isset($organization)) && $organization == "indivisual") {echo "selected";} ?> >Indivisual</option>
+            <option value="school" <?php if ($organization == "school"){ echo "selected";} ?> >School</option>
+            <option value="non-profit" <?php if ($organization == "non-profit"){ echo "selected";} ?> >Non-Profit</option>
+            <option value="profit" <?php if ($organization == "profit") {echo "selected";} ?> >Profit</option>
+            <option value="indivisual" <?php if ($organization == "indivisual") {echo "selected";} ?> >Indivisual</option>
           </select>
         </td>
       </tr>
